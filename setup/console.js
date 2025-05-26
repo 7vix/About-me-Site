f0b3ba9c23bf6b20f819530019848354d5820197
7vix <vixstaffapplication@gmail.com>
2025-05-26 18:49:37 +0200

    Add files via upload

---
+++ setup/console.js
@@ -0,0 +1,7 @@
+console.log("👋 Welcome to 7vix's Portfolio")
+console.log("🍍 APACHE LICENSE 2.0v")
+console.log("🔏 Restricted and Secure")
+
+setTimeout(() => {
+    console.log("Connected to 7vix's portfolio");
+},  2000);
